939fcbdc95f0819a445b6c5208faa6f5f2c4fdf4
Kelli Margaret <kelli.sommerdyke@gmail.com>
2015-08-21 11:18:10 -0700

    Add functions for Client and Stylist classes

--- src/Client.php
+++ src/Client.php
@@ -43,17 +43,64 @@
     {
         return $this->name;
     }
+
     function getId()
     {
       return $this->id;
     }
+
     function getStylistId()
     {
         return $this->stylist_id;
     }
+
+    function getPhone()
+    {
+        return $this->phone;
+    }
+
     function getNextVisit()
     {
         return $this->next_visit;
+    }
+
+    //Save function
+    function save()
+    {
+      $GLOBALS['DB']->exec("INSERT INTO client (name, stylist_id, phone, next_visit) VALUES ('{$this->getClientName()}', {$this->getStylistId()}, '{$this->getPhone()}', '{$this->getNextVisit()}');");
+      $this->id = $GLOBALS['DB']->lastInsertId();
+    }
+    //Get All function
+    static function getAll()
+    {
+        $returned_tasks = $GLOBALS['DB']->query("SELECT * FROM client ORDER BY next_visit;");
+        $tasks = array();
+        foreach($returned_client as $client) {
+          $name = $name['name'];
+          $id = $client['id'];
+          $stylist_id = $client['stylist_id'];
+          $new_client = new Client($name, $stylist_id, $phone, $next_visit, $id);
+          array_push($clients, $new_client);
+        }
+        return $clients;
+    }
+    //Delete ALl function
+    static function deleteAll()
+    {
+      $GLOBALS ['DB']->exec("DELETE FROM clients;");
+    }
+    //Find function
+    static function find($search_id)
+    {
+      $found_client = null;
+      $clients = Client::getAll();
+      foreach($clients as $client) {
+        $client_id = $client->getId();
+        if ($client_id == $search_id) {
+          $found_client = $client;
+        }
+      }
+      return $found_client;
     }
 
 ?>
--- src/Stylist.php
+++ src/Stylist.php
@@ -23,15 +23,64 @@
         {
             return $this->name;
         }
-        
+
         function getClients()
         {
             $clients = array();
-            $returned_clients = $GLOBALS['DB']->query("SELECT * FROM clients WHERE client_id = {$this->getId()};");
-            array_push($clients, $new_clients);
+            $returned_clients = $GLOBALS['DB']->query("SELECT * FROM clients WHERE stylist_id = {$this->getId()} ORDER BY next_visit;");
+            foreach($returned_clients as $client) {
+                $name = $client['name'];
+                $stylist_id = $client['stylist_id'];
+                $next_visit = $client['next_visit'];
+                $id = $task['id'];
+                $new_client= new Client($name, $stylist_id, $next_visit, $id);
+                array_push($clients, $new_client);
+            }
+            return $clients;
         }
 
-        return $clients;
+        function getId()
+        {
+            return $this->id;
+        }
+
+        //Save function
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO stylsits (name) VALUES ('{$this->getStylistName()}')");
+            $this->id= $GLOBALS['DB']->lastInsertId();
+        }
+        //Static functions
+        static function getAll()
+        {
+            $returned_stylists = $GLOBALS['DB']->query("SELECT * FROM stylists;");
+            $stylists = array();
+            foreach($returned_stylists as $stylist) {
+                $name = $stylists['name'];
+                $new_stylist = new Stylist($name, $id);
+                $id = $stylist['id'];
+                array_push($stylists, $new_stylist);
+            }
+            return $stylists;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM stylists;");
+        }
+        static function find($search_id)
+        {
+            $found_stylist = null;
+            $stylists = Stylist::getAll();
+            foreach($stylists as $stylist) {
+                $stylist_id = $stylist->getId();
+                if ($stylist_id == $search_id) {
+                $found_stylist = $stylist;
+                }
+            }
+            return $found_stylist;
+        }
     }
+
 
 ?>
